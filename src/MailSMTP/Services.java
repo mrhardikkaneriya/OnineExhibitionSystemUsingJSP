f5e55def44d4b3163027e0b2c9964853e349ba71
Hardik Kaneriya <86786467+mrhardikkaneriya@users.noreply.github.com>
2023-01-04 14:54:06 +0530

    Update Services.java

--- src/MailSMTP/Services.java
+++ src/MailSMTP/Services.java
@@ -18,8 +18,8 @@ public class Services
 	 
         System.out.println("Start");
         
-        final String username  = "onlinejobportal2021@gmail.com";//Sender Email
-		final String password   = "ukcq itxv kgil nvbl";//Sender Password
+        final String username  = "email@gmail.com";//Sender Email
+		final String password   = "password";//Sender Password
 		
 		System.out.println("1");
 		Properties props = new Properties();
