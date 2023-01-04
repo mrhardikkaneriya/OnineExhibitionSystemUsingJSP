fc0c5c5544e8de923ad577a10f620c75229066a5
Hardik Kaneriya <86786467+mrhardikkaneriya@users.noreply.github.com>
2023-01-04 14:54:56 +0530

    Update PaytmConstants.java

--- src/payment/PaytmConstants.java
+++ src/payment/PaytmConstants.java
@@ -2,8 +2,8 @@ package payment;
 
 public class PaytmConstants {
 	
-	public final static String MID = "iKNhwW40172988775877";
-	public final static String MERCHANT_KEY = "GGDCTkYaggY4hXg&";
+	public final static String MID = "xxx";
+	public final static String MERCHANT_KEY = "xxx";
 
 	
 	public final static String INDUSTRY_TYPE_ID = "Retail";
